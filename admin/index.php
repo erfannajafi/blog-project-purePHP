43209950cb0c2eb74c5376b6b39576c60f6b3bd4
erfannajafi <najafi.e79@gmail.com>
2023-07-29 14:12:20 +0330

    complete dashboard for admin

--- admin/index.php
+++ admin/index.php
@@ -1,65 +1,59 @@
-<!DOCTYPE html>
-<html lang="fa" dir="rtl">
+<?php 
+include("./include/header.php");
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./css/admin.css" />
-    <title>Blog WebProg</title>
-</head>
+if(isset($_GET['entity']) && isset($_GET['action']) && isset($_GET['id']) )
+ {
+    $entity = $_GET['entity'];
+    $action = $_GET['action'];
+    $id = $_GET['id'];
 
-<body>
-    <!-- Header -->
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">WebProg.ir</a>
+    if ($action == "delete") 
+    {
+        if ($entity == "post") 
+        {
+            $query = $db->prepare("DELETE FROM posts WHERE id = :id");
+        }
+        elseif ($entity == "category") 
+        {
+            $query = $db->prepare("DELETE FROM categories WHERE id = :id");
+        }
+        elseif($entity == "comment") 
+        {
+            $query = $db->prepare("DELETE FROM comments WHERE id = :id");
+        }
 
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="logout.php">خروج</a>
-            </li>
-        </ul>
-    </nav>
+        $query->execute(['id' => $id]);
+
+    }
+
+    elseif ($action == "approve")  { 
+        $query = $db->prepare("UPDATE comments SET status='1' WHERE id=:id");
+        $query->execute(['id' => $id]);
+    } 
+}
+
+
+$query_posts = "SELECT * FROM posts ORDER BY id DESC";
+$posts = $db->query($query_posts);
+
+$query_comments = "SELECT * FROM comments WHERE status='0' ORDER BY id DESC";
+$comments = $db->query($query_comments);
+
+$query_categories = "SELECT * FROM categories ORDER BY id DESC";
+$categories = $db->query($query_categories);
+
+?>
 
     <div class="container-fluid">
         <div class="row">
 
-            <!-- Sidebar -->
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.php">
-                                <i class="fas fa-home"></i>
-                                داشبورد
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="post.php">
-                                <i class="fas fa-file-image"></i>
-                                مقالات
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="category.php">
-                                <i class="fas fa-folder-open"></i>
-                                دسته بندی
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="comment.php">
-                                <i class="fas fa-comments"></i>
-                                کامنت
-                            </a>
-                        </li>
+        <?php
+        include("./include/sidebar.php");
 
-                    </ul>
+        ?>
 
-                </div>
-            </nav>
+
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -78,36 +72,33 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> 1 </td>
-                                <td> لورم ایپسوم 1 </td>
-                                <td> متین سیدی </td>
+                            <?php 
+                            if($posts->rowCount() > 0) {
+                                foreach($posts as $post) {
+                                    ?>                              
+                                    <tr>
+                                        <td> <?php echo $post['id']; ?> </td>
+                                        <td> <?php echo $post['title']; ?> </td>
+                                        <td> <?php echo $post['author']; ?> </td>
+                                        <td>
+                                            <a href="edit_post.php?id=<?php echo $post['id']; ?>" class="btn btn-outline-info">ویرایش</a>
+                                            <a href="index.php?entity=post&action=delete&id=<?php echo $post['id']; ?>" class="btn btn-outline-danger">حذف</a>
+                                        </td>
+                                    </tr>
 
-                                <td>
-                                    <a href="edit_post.php" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 2 </td>
-                                <td> لورم ایپسوم 2 </td>
-                                <td> علی شیخ </td>
+                            <?php
+                                }
+                            }  else {
+                                ?>
+                                <div class="alert alert-danger" role="alert">
+                                    پستی برای نمایش وجود ندارد.
+                                </div>
 
-                                <td>
-                                    <a href="edit_post.php" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 3 </td>
-                                <td> لورم ایپسوم 3 </td>
-                                <td> علی شیخ </td>
+                                <?php
+                            }
+                            ?>
 
-                                <td>
-                                    <a href="edit_post.php" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
+
                         </tbody>
                     </table>
                 </div>
@@ -124,36 +115,32 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> 1 </td>
-                                <td> نیما </td>
-                                <td> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </td>
+                        <?php 
+                            if($comments->rowCount() > 0) {
+                                foreach($comments as $comment) {
+                                    ?>                              
+                                    <tr>
+                                        <td> <?php echo $comment['id']; ?> </td>
+                                        <td> <?php echo $comment['name']; ?> </td>
+                                        <td> <?php echo $comment['comment']; ?> </td>
+                                        <td>
+                                            <a href="index.php?entity=comment&action=approve&id=<?php echo $comment['id']; ?>" class="btn btn-outline-success">تایید</a>
+                                            <a href="index.php?entity=comment&action=delete&id=<?php echo $comment['id']; ?>" class="btn btn-outline-danger">حذف</a>
+                                        </td>
+                                    </tr>
 
-                                <td>
-                                    <a href="#" class="btn btn-outline-success">تایید</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 2 </td>
-                                <td> علی </td>
-                                <td> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </td>
+                            <?php
+                                }
+                            } else {
+                                ?>
+                                <div class="alert alert-danger" role="alert">
+                                    کامنتی برای نمایش وجود ندارد.
+                                </div>
 
-                                <td>
-                                    <a href="#" class="btn btn-outline-success">تایید</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 3 </td>
-                                <td> صادق </td>
-                                <td> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </td>
-
-                                <td>
-                                    <a href="#" class="btn btn-outline-success">تایید</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
+                                <?php
+                            }
+                            ?>
+ 
                         </tbody>
                     </table>
                 </div>
@@ -169,30 +156,32 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> 1 </td>
-                                <td> دسته 1 </td>
-                                <td>
-                                    <a href="edit_category" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 2 </td>
-                                <td> دسته 2 </td>
-                                <td>
-                                    <a href="edit_category" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 3 </td>
-                                <td> دسته 3 </td>
-                                <td>
-                                    <a href="edit_category" class="btn btn-outline-info">ویرایش</a>
-                                    <a href="#" class="btn btn-outline-danger">حذف</a>
-                                </td>
-                            </tr>
+                        <?php 
+                            if($categories->rowCount() > 0) {
+                                foreach($categories as $category) {
+                                    ?>                              
+                                    <tr>
+                                        <td> <?php echo $category['id']; ?> </td>
+                                        <td> <?php echo $category['title']; ?> </td>
+                                        <td>
+                                            <a href="edit_category.php&id=<?php echo $category['id']; ?>" class="btn btn-outline-info">ویرایش</a>
+                                            <a href="index.php?entity=category&action=delete&id=<?php echo $category['id']; ?>" class="btn btn-outline-danger">حذف</a>
+                                        </td>
+                                    </tr>
+
+                            <?php
+                                }
+                            } else {
+                                ?>
+                                <div class="alert alert-danger" role="alert">
+                                    دسته ای برای نمایش وجود ندارد.
+                                </div>
+
+                                <?php
+                            }
+                            ?>
+
+
                         </tbody>
                     </table>
                 </div>
